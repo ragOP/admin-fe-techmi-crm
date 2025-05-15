57ab03e2adaf3f732913d2f14f98bf3eebababcd
Kashif <kashifdeshmukh786@gmail.com>
2025-05-15 23:05:59 +0530

    Updated backend URL

--- src/api/enpoints/index.js
+++ src/api/enpoints/index.js
@@ -3,7 +3,7 @@ export const isDev = () => {
 };
 
 export const BACKEND_URL = isDev()
-  ? "http://localhost:8000"
+  ? "https://2c66-84-247-129-99.ngrok-free.app"
   : "https://2c66-84-247-129-99.ngrok-free.app";
 
 export const endpoints = {
