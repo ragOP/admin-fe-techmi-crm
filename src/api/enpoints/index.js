8b2a09f595f345e3986f88f08f613100e171e6b1
Kashif <kashifdeshmukh786@gmail.com>
2025-05-25 22:19:17 +0530

    Url changed and pushed

--- src/api/enpoints/index.js
+++ src/api/enpoints/index.js
@@ -3,7 +3,7 @@ export const isDev = () => {
 };
 
 export const BACKEND_URL = isDev()
-  ? "http://localhost:8000"
+  ? "https://393d-84-247-129-99.ngrok-free.app"
   : "https://393d-84-247-129-99.ngrok-free.app";
 
 export const endpoints = {
